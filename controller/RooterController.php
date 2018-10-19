5f62f5c5f7c45f30a864de8ae914117be5fb89b9
Pedro Dal Bianco <dalbianco.pedro@gmail.com>
2018-10-18 21:32:30 -0300

    Agregada funcion para obtener usuarios por varios parametros

--- controller/RooterController.php
+++ controller/RooterController.php
@@ -23,7 +23,7 @@ class RooterController {
   public function redireccionar($comando){
 
     session_start();
-    
+
     if(!isset($_SESSION["id"])) {
 
       switch ($comando) {
@@ -31,7 +31,7 @@ class RooterController {
         case 'iniciar-sesion':
           SessionController::singleton()->login();
           break;
-      
+
         case 'login':
           SessionController::singleton()->redireccionarLogin();
           break;
@@ -49,16 +49,16 @@ class RooterController {
     } else {
 
       switch ($comando) {
-      
+
         /*
-        ** Session rotter
+        ** Session rooter
         */
-  
+
         case 'iniciar-sesion':
           // MODIFICAR: DEBERIA APARECER QUE LA SESSION ESTA INICIADA
           echo 'Session ya iniciada';
           break;
-      
+
         case 'login':
           // MODIFICAR: DEBERIA APARECER QUE LA SESSION ESTA INICIADA
           echo 'Session ya iniciada';
@@ -67,27 +67,27 @@ class RooterController {
         case 'cerrar-session':
           SessionController::singleton()->closeSession();
           break;
-  
+
         /*
         ** Administrador rooter
         */
-        
+
         case 'configuracion':
           AdministradorController::singleton()->redireccionarConfiguracion();
           break;
-        
+
         case 'sitio-en-mantenimiento':
           AdministradorController::singleton()->redireccionarSitioEnMantenimiento();
           break;
-  
+
         /*
         ** Usuarios rooter
-        */ 
-  
+        */
+
         case 'busqueda-usuarios':
           UsuarioController::singleton()->redireccionarBusquedaUsuarios();
           break;
-  
+
         /*
         ** Default
         */
@@ -95,7 +95,7 @@ class RooterController {
         case '':
           SessionController::singleton()->redireccionarHome();
           break;
-        
+
         default:
           // MODIFICAR: ACA DEBERIA TIRAR ERROR
           SessionController::singleton()->redireccionarHome();
