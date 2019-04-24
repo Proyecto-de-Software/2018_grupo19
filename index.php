cd73a955c12ed307b9e1a3c284baf1524a0eb812
Blas <blasbutera69@gmail.com>
2019-04-24 19:56:29 -0300

    Comenzando nuevo proyecto Laravel

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'composer/vendor/autoload.php';
-require_once 'controller/RooterController.php';
-
-if (!isset($_GET['comando'])) {$_GET['comando'] = '';}
-RooterController::singleton()->redireccionar($_GET['comando']);
-
-?>
