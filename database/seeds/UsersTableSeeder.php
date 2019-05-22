4bc8cc1fc67f42f68de212f85ebb8f49d3715757
fedediclaudio <37820912+fedediclaudio@users.noreply.github.com>
2019-05-22 12:07:31 -0300

    Implementacion de Roles y Permisos + Seed + Verficacion en rutas

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,10 +12,22 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
+        User::create([
             'name'=>'root',
             'email'=>'root@hak.com',
             'password'=>bcrypt('123456')
-        ]);
+        ])->assignRole('Administrador');
+
+        User::create([
+            'name' => 'equipoGuardia',
+            'email' => 'equipoguardia@hak.com',
+            'password' => bcrypt('123456'),
+        ])->assignRole('Equipo de Guardia');
+
+        User::create([
+            'name' => 'atencion',
+            'email' => 'atencion@hak.com',
+            'password' => bcrypt('123456'),
+        ])->assignRole('Personal de Atencion');
     }
 }
