4715e0bdfb4fa5965ad5e593362e3426ce565191
fedediclaudio <37820912+fedediclaudio@users.noreply.github.com>
2019-06-01 18:19:46 -0300

    Back de busqueda de pacientes

--- app/Paciente.php
+++ app/Paciente.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Paciente extends Model
 {
-    
+
     public function localidad() {
         return $this->belongsTo('App\Localidad');
     }
@@ -25,6 +25,36 @@ class Paciente extends Model
 
     public function obra_social() {
         return $this->belongsTo('App\ObraSocial');
+    }
+
+    public function scopeNombre($query, $nombre) {
+        if($nombre) {
+            $query->where('nombre',  'like', $nombre . '%');
+        }
+    }
+
+    public function scopeApellido($query, $apellido) {
+        if($apellido) {
+            $query->where('apellido', 'like', $apellido . '%');
+        }
+    }
+
+    public function scopeNroDoc($query, $nro_doc) {
+        if($nro_doc) {
+            $query->where('documento', $nro_doc);
+        }
+    }
+
+    public function scopeTipoDoc($query, $tipo_doc) {
+        if($tipo_doc != 0) {
+            $query->where('tipo_doc_id', $tipo_doc);
+        }
+    }
+
+    public function scopeNroHistoriaClinica($query, $nro_hc) {
+        if($nro_hc) {
+            $query->where('nro_historia_clinica', $nro_hc);
+        }
     }
 
 }
--- resources/views/pacientes/index.blade.php
+++ resources/views/pacientes/index.blade.php
@@ -4,36 +4,54 @@
     <div class="form-group">
         <label for="filtro-nombre" class="col-sm-1 control-label">Nombre</label>
         <div class="col-sm-5">
-            <input type="text" name="nombre" id="filtro-username" class="form-control">
+            <input type="text" name="nombre" id="filtro-username" class="form-control" value="{{app('request')->input('nombre')}}">
         </div>
         <label for="filtro-apellido" class="col-sm-1 control-label">Apellido</label>
         <div class="col-sm-5">
-            <input type="text" name="apellido" id="filtro-active" class="form-control">
+            <input type="text" name="apellido" id="filtro-active" class="form-control" value="{{app('request')->input('apellido')}}">
         </div>
     </div>
     <div class="form-group">
         <label for="filtro-nro-doc" class="col-sm-2 control-label">Numero Doc.</label>
         <div class="col-sm-5">
-            <input type="text" name="nro-doc" id="filtro-nro-doc" class="form-control">
+            <input type="text" name="nro-doc" id="filtro-nro-doc" class="form-control" value="{{app('request')->input('nro-doc')}}">
         </div>
         <label for="filtro-tipo-doc" class="col-sm-2 control-label">Tipo Doc.</label>
         <div class="col-sm-3">
             <select name="tipo-doc" id="filtro-tipo-doc" class="form-control">
-                <option value="0" selected></option>
-                <option value="1">DNI</option>
-                <option value="2">LC</option>
-                <option value="3">CI</option>
-                <option value="4">LE</option>
-                <option value="5">Pasaporte</option>
+                <option value="0" @if (app('request')->input('tipo-doc') == 0)
+                        selected
+                @endif ></option>
+                <option value="1" @if (app('request')->input('tipo-doc') == 1)
+                    selected
+                @endif>DNI</option>
+                <option value="2" @if (app('request')->input('tipo-doc') == 2)
+                    selected
+                @endif>LC</option>
+                <option value="3" @if (app('request')->input('tipo-doc') == 3)
+                    selected
+                @endif>CI</option>
+                <option value="4" @if (app('request')->input('tipo-doc') == 4)
+                    selected
+                @endif>LE</option>
+                <option value="5" @if (app('request')->input('tipo-doc') == 5)
+                    selected
+                @endif>Pasaporte</option>
             </select>
         </div>
     </div>
     <div class="form-group">
         <label for="filtro-historia-clinica" class="col-sm-4 control-label">Numero de Historia Clinica</label>
         <div class="col-sm-7">
-            <input type="text" name="historia-clinica" id="filtro-historia-clinica" class="form-control">
+            <input type="text" name="historia-clinica" id="filtro-historia-clinica" class="form-control" value="{{app('request')->input('historia-clinica')}}">
         </div>
     </div>
+@endsection
+
+@section('boton_listado_completo')
+    <a href="{{ url('pacientes') }}"><button class="btn btn-default">
+        <i>Todos los pacientes</i>
+    </button></a>
 @endsection
 
 @section('item-list')
