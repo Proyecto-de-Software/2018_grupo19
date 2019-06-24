420addb9b0f0f153e13a8e6d63230497ee80f3fc
Blas <blasbutera69@gmail.com>
2019-06-24 00:29:58 -0300

    alta de instituciones via json REST ok

--- app/Http/Controllers/InstitucionController.php
+++ app/Http/Controllers/InstitucionController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Institucion;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Validator;
 
 class InstitucionController extends Controller
 {
@@ -19,19 +20,79 @@ class InstitucionController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * Store a newly created resource in storage.
      *
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function store(Request $request)
     {
-        return Institucion::where('id', $id)->get();
+        if($this->validate_i($request)->fails()){
+
+            return response("Fallo",400);
+        }else{
+            $institucion = new Institucion;
+            $institucion->nombre = $request->nombre;
+            $institucion->director = $request->director;
+            $institucion->telefono = $request->telefono;
+            $institucion->longitud = $request->longitud;
+            $institucion->latitud = $request->latitud;
+            $institucion->region_sanitaria_id = $request->region_sanitaria_id;
+            $institucion->tipo_institucion_id = $request->tipo_institucion_id;
+            $institucion->save();
+
+            return response("Ok",200);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Institucion  $institucion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Institucion $institucion)
+    {
+        return $institucion;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Institucion  $institucion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Institucion $institucion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Institucion  $institucion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Institucion $institucion)
+    {
+        //
     }
 
     public function showByRegion($idRegion)
     {
-        Log::info("Entrando showbyregion con region $idRegion");
         return Institucion::where('region_sanitaria_id', $idRegion)->get();
+    }
+
+    private function validate_i($request){
+        return Validator::make($request->json()->all(),[
+            'nombre' => 'required|string|min:5|max:50',
+            'director' => 'required|string|min:5|max:50',
+            'telefono' => 'required|string|min:6|max:50',
+            'longitud' => 'required|string|min:5|max:50',
+            'latitud' => 'required|string|min:5|max:50',
+            'region_sanitaria_id' => 'required|integer|exists:region_sanitarias,id',
+            'tipo_institucion_id' => 'required|integer|exists:tipo_institucions,id',
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('instituciones', 'InstitucionController@index');
 Route::get('instituciones/{institucion}', 'InstitucionController@show');
 Route::get('instituciones/region-sanitaria/{regionsanitaria}', 'InstitucionController@showByRegion');
+Route::get('institucionesq', 'InstitucionController@store');
